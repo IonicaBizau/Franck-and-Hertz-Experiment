ce38b2224844dac2779e573af0d0b6e7e6ef35d6
Ionică Bizău <bizauionica@yahoo.com>
2014-03-19 13:04:07 +0200

    Added updateResult function and change handler for voltmeter

--- js/functions.js
+++ js/functions.js
@@ -3,8 +3,27 @@
  *
  * */
 $(document).ready(function () {
+
+    // initial animation
     $(".container").css("opacity", "0").animate({
         opacity: 1
       , top: "50%"
     }, 1000);
+
+    /**
+     * private: updateResult
+     *   This function updates the result when the voltmeter
+     *   value is changed.
+     *
+     *    Arguments
+     *      @value: the value of the voltmeter
+     *
+     */
+    function updateResult (value) {
+    }
+
+    // change handler for voltmeter input
+    $(".vol input[type='number']").on("change", function () }
+        updateResult($(this).val()) 
+    });
 });
