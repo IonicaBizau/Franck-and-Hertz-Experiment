edeae1dbdf89148567e11ba6c75cd0bd93619574
Ionică Bizău <bizauionica@yahoo.com>
2014-03-25 11:51:24 +0200

    Move cursor using simple draggable plugin. Update values

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,59 @@
+var SimpleDraggable = function (selector, options) {
+
+    options.onStart = options.onStart || function () {};
+    options.onStop = options.onStop || function () {};
+    options.onDrag = options.onDrag || function () {};
+
+    var allElms = document.querySelectorAll (selector);
+    for (var i = 0; i < allElms.length; ++i) {
+        (function (cEl) {
+            // document.body.appendChild(cEl);
+            cEl.style.position = "absolute";
+            cEl._simpleDraggable = {
+               drag: false
+            }
+
+            cEl.addEventListener("mousedown", function (e) {
+                cEl._simpleDraggable.drag = true;
+                cEl._simpleDraggable.mousePos = {
+                    x: e.clientX
+                  , y: e.clientY
+                }
+                cEl._simpleDraggable.elPos = {
+                    x: cEl.offsetLeft
+                  , y: cEl.offsetTop
+                }
+            });
+
+            cEl.addEventListener("mouseup", function (e) {
+                cEl._simpleDraggable.drag = false;
+                options.onStart.call(this, e, cEl);
+            });
+
+            document.body.addEventListener("mouseout", function (e) {
+                cEl._simpleDraggable.drag = false;
+                options.onStop.call(this, e, cEl);
+            });
+
+            document.body.addEventListener("mousemove", function (e) {
+                if (!cEl._simpleDraggable.drag) { return; }
+
+                if (options.onDrag.call(this, e, cEl) === false) {
+                    return;
+                }
+
+                if (!options.onlyY) {
+                    cEl.style.left = (cEl._simpleDraggable.elPos.x + e.clientX - cEl._simpleDraggable.mousePos.x) + "px";
+                }
+                if (!options.onlyX) {
+                    cEl.style.top = (cEl._simpleDraggable.elPos.y + e.clientY - cEl._simpleDraggable.mousePos.y) + "px";
+                }
+
+            })
+        })(allElms[i])
+    }
+};
+
 /*
  *  ....
  *
@@ -9,6 +65,36 @@ $(document).ready(function () {
         opacity: 1
       , top: "50%"
     }, 1000);
+
+    var max = 226
+      , min = 98
+      ;
+
+    $("img").on("dragstart", function () {
+        return false;
+    });
+
+    SimpleDraggable(".cursor", {
+        onlyX: true
+      , onDrag: function (e, cEl) {
+
+            var value = (cEl.offsetLeft - min) / 8.6;
+            if (value < 0) { value = 0; }
+            $(".vol input").val(value.toFixed(2));
+            updateResult(value);
+
+
+            if (cEl.offsetLeft > max) {
+                cEl.style.left = (parseInt(cEl.style.left) - 1) + "px";
+                return false;
+            }
+
+            if (cEl.offsetLeft < min) {
+                cEl.style.left = (parseInt(cEl.style.left) + 1) + "px";
+                return false;
+            }
+        }
+    });
 
     /**
      * private: updateResult
@@ -45,7 +131,7 @@ $(document).ready(function () {
 
 
     // change handler for voltmeter input
-    $(".vol input[type='number']").on("change", function () {
+    $(".vol input").on("change", function () {
         updateResult(Number($(this).val()));
     });
 });
